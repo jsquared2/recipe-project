2944ac3ab035c7bea95b206df1e7e7b7d4ef1d1f
John Juan <john.juan@sap.com>
2019-01-06 08:01:53 -0500

    Initial Spring Boot project commit.

---
+++ src/main/java/com/sap/fs/poc/recipeproject/RecipeProjectApplication.java
@@ -0,0 +1,14 @@
+package com.sap.fs.poc.recipeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeProjectApplication.class, args);
+	}
+
+}
+
