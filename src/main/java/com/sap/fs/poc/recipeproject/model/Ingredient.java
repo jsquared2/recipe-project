0570e9d75b191a0f14d253bd2e4dcf24abc39893
John Juan <john.juan@sap.com>
2019-01-07 20:58:19 -0500

    Completed assignment for one to one JPA relationship.

--- src/main/java/com/sap/fs/poc/recipeproject/model/Ingredient.java
+++ src/main/java/com/sap/fs/poc/recipeproject/model/Ingredient.java
@@ -3,10 +3,12 @@ package com.sap.fs.poc.recipeproject.model;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Ingredient {
@@ -21,6 +23,9 @@ public class Ingredient {
 
 	@ManyToOne
 	private Recipe recipe;
+
+	@OneToOne(fetch = FetchType.EAGER)
+	private UnitOfMeasure uom;
 
 	public Long getId() {
 		return id;
@@ -52,6 +57,14 @@ public class Ingredient {
 
 	public void setRecipe(Recipe recipe) {
 		this.recipe = recipe;
+	}
+
+	public UnitOfMeasure getUom() {
+		return uom;
+	}
+
+	public void setUom(UnitOfMeasure uom) {
+		this.uom = uom;
 	}
 
 }
